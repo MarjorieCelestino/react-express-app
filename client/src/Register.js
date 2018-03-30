35a3d5b523589e4b34ef4a618f56317919853fd7
Marjorie Celestino <marjorie_celestino@hotmail.com>
2018-03-30 20:55:35 -0300

    register layout and image

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,55 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class Register extends Component {
   render() {
     return (
-    <div className="Login">
+    <div className="Register">
       <head>
       	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/>
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 		<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
       </head>
       	<nav class="navbar navbar-default navbar-fixed-top blank-space">
+    		<ul class="nav navbar-nav navbar-right">
+      			<li class="btn-log"><a href="#contact">Sign in</a></li>
+    		</ul>
 		</nav>
 	    <div class="container">
         	<div class="row">
 				<div class="col-md-6 col-md-offset-3">
 					<div class="panel panel-login">
 						<div class="panel-heading">
-						 	<a class="active">Welcome</a>
+							<a class="active">Create Account</a>
 						<hr/>
 						</div>
 						<div class="panel-body">
 							<div class="row">
 								<div class="col-lg-12">
-									<form id="login-form" method="post" role="form">
+									<form id="register-form">
 										<div class="form-group">
-											<input type="text" name="email" id="email" tabindex="1" class="form-control" placeholder="E-mail" value=""/>
+											<input type="text" name="name" id="name" tabindex="1" class="form-control" placeholder="Name" value=""/>
+										</div>
+										<div class="form-group">
+											<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value=""/>
 										</div>
 										<div class="form-group">
 											<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password"/>
 										</div>
 										<div class="form-group">
-											<div class="row">
-												<div class="col-lg-12">
-													<div class="text-center">
-														<a tabindex="5" class="forgot-password">Forgot Password?</a>
-													</div>
-												</div>
-											</div>
+											<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password"/>
 										</div>
 										<div class="form-group">
 											<div class="row">
 												<div class="col-sm-6 col-sm-offset-3">
-													<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Sign in"/>
-												</div>
-											</div>
-										</div>
-										<div class="form-group">
-											<div class="row">
-												<div class="col-sm-6 col-sm-offset-3">
-													<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="create account"/>
+													<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register now"/>
 												</div>
 											</div>
 										</div>
@@ -66,4 +59,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Register;
